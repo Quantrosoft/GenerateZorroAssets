5e4258422a63319ef8b49c9e23f9f7f82a7ae7df
Heinrich Munz <Heinrich.Munz@Munz-Endeavors.com>
2024-10-03 15:12:10 +0200

    Write out only existing symbols

--- GenerateZorroAssets/GenerateZorroAssets.cs
+++ GenerateZorroAssets/GenerateZorroAssets.cs
@@ -66,94 +66,97 @@ namespace cAlgo.Robots
       {
          //if (Time.Hour >= 9)
          //   if (Time.Hour <= 16)
-               if (mSymbolCount < mSymbolsCount)
+         if (mSymbolCount < mSymbolsCount)
+         {
+            mExcludeSplit = ExcludeCsv.Split(",");
+            Symbol sym = null;
+
+            var isExcluded = false;
+            foreach (var ex in mExcludeSplit)
+               if ("" != ex && Symbols[mSymbolCount].ToLower().Contains(ex.ToLower()))
                {
-                  mExcludeSplit = ExcludeCsv.Split(",");
-                  Symbol sym = null;
-
-                  var isExcluded = false;
-                  foreach (var ex in mExcludeSplit)
-                     if ("" != ex && Symbols[mSymbolCount].ToLower().Contains(ex.ToLower()))
-                     {
-                        Print((mSymbolCount + 1).ToString() + " " + Symbols[mSymbolCount] + " excluded");
-                        isExcluded = true;
-                        break;
-                     }
-
-                  if (!isExcluded)
-                  {
-                     Print((mSymbolCount + 1).ToString() + " " + Symbols[mSymbolCount]);
-                     sym = Symbols.GetSymbol(Symbols[mSymbolCount]);
-
-                     if (null != sym
-                           && sym.IsTradingEnabled
-                           && !Double.IsNaN(sym.Spread))  // some sybols have a Spread of NaN ?!)
-                        mSymInfos.Add(new SymInfo
-                        {
-                           Symbol = sym
-                        });
-                     else
-                        Print((mSymbolCount + 1).ToString() + " " + Symbols[mSymbolCount] + " invalid");
-                  }
-
-                  mSymbolCount++;
+                  Print((mSymbolCount + 1).ToString() + " " + Symbols[mSymbolCount] + " excluded");
+                  isExcluded = true;
+                  break;
                }
-               else if (mSymInfos[^1].SpreadTickCount < 100)
-               {
-                  for (int i = 0; i < mSymInfos.Count; i++)
+
+            if (!isExcluded)
+            {
+               Print((mSymbolCount + 1).ToString() + " " + Symbols[mSymbolCount]);
+               sym = Symbols.GetSymbol(Symbols[mSymbolCount]);
+
+               if (null != sym
+                     && sym.IsTradingEnabled
+                     && !Double.IsNaN(sym.Spread))  // some sybols have a Spread of NaN ?!)
+                  mSymInfos.Add(new SymInfo
                   {
-                     mSymInfos[i].SpreadSum += mSymInfos[i].Symbol.Spread;
-                     mSymInfos[i].SpreadTickCount++;
-
-                     mSymInfos[i].AvgSpread = mSymInfos[i].SpreadSum / mSymInfos[i].SpreadTickCount;
-                  }
-               }
-               else if (mWriteCount < mSymbolsCount)
-               {
-                  // write Zorro Assets file i.e Assets_Pepperstone_Live.csv
-                  if (null == mZorroAssetsWriter)
-                  {
-                     var zorroAssetsPath = Path.Combine(ZorroHistoryPath, "Assets_"
-                        + Account.BrokerName + "_" + (Account.IsLive ? "Live" : "Demo") + ".csv");
-                     mZorroAssetsWriter = new StreamWriter(File.OpenWrite(zorroAssetsPath));
-                     mZorroAssetsWriter.WriteLine("Name,Price,Spread,RollLong,RollShort,PIP,PIPCost,MarginCost,Market,Multiplier,Commission,Symbol,Leverage,Lotsize,Base,Quote");
-                  }
-
-                  Print((mWriteCount + 1).ToString() + " Writing " + mSymInfos[mWriteCount].Symbol.Name);
-
-                  var digits = mSymInfos[mWriteCount].Symbol.Digits;
-                  var line = mSymInfos[mWriteCount].Symbol.Name                                        // User symbol name
-                     + "," + mSymInfos[mWriteCount].Symbol.Bid.ToString($"F{digits}", UsCulture)       // Price
-                     + "," + mSymInfos[mWriteCount].AvgSpread.ToString($"F{digits + 1}", UsCulture)    // Spread
-                     + "," + mSymInfos[mWriteCount].Symbol.SwapLong.ToString($"F{5}", UsCulture)       // Swap long
-                     + "," + mSymInfos[mWriteCount].Symbol.SwapShort.ToString($"F{5}", UsCulture)      // Swap short
-                     + "," + mSymInfos[mWriteCount].Symbol.PipSize.ToString($"F{digits}", UsCulture)   // Pip size
-                     + "," + (mSymInfos[mWriteCount].Symbol.PipValue * mSymInfos[mWriteCount].Symbol.LotSize)    // Value of 1 pip profit or loss per lot
-                        .ToString($"F{8}", UsCulture)
-                     + "," + mSymInfos[mWriteCount].Symbol.GetEstimatedMargin(TradeType.Buy, mSymInfos[mWriteCount].Symbol.LotSize)   // Margin
-                        .ToString($"F{2}", UsCulture)
-                     // Market open hours ZZZ:HHMM-HHMM, for instance EST:0930-1545
-                     + ",UTC:" + mSymInfos[mWriteCount].Symbol.MarketHours.Sessions[1].StartTime.ToString(@"hh\:mm")
-                        + "-" + mSymInfos[mWriteCount].Symbol.MarketHours.Sessions[1].EndTime.ToString(@"hh\:mm")
-                     + "," + mSymInfos[mWriteCount].Symbol.VolumeInUnitsMin.ToString($"F{8}", UsCulture)  // Min volume
-                     + "," + mSymInfos[mWriteCount].Symbol.Commission.ToString($"F{2}", UsCulture)     // Commission
-                     + "," + mSymInfos[mWriteCount].Symbol                                             // Broker symbol Name
-                     + "," + ((mSymInfos[mWriteCount].Symbol.DynamicLeverage != null
-                              && mSymInfos[mWriteCount].Symbol.DynamicLeverage.Count > 0
-                                 ? (int)mSymInfos[mWriteCount].Symbol.DynamicLeverage[0].Leverage       // Leverage
-                                 : ""))
-                     + "," + mSymInfos[mWriteCount].Symbol.LotSize.ToString($"F{2}", UsCulture)        // LotSize
-                     + "," + mSymInfos[mWriteCount].Symbol.BaseAsset                                   // Base currency
-                     + "," + mSymInfos[mWriteCount].Symbol.QuoteAsset;                                 // Quote currency
-
-                  mZorroAssetsWriter.WriteLine(line);
-                  mWriteCount++;
-               }
+                     Symbol = sym
+                  });
                else
                {
-                  mZorroAssetsWriter.Close();
-                  Stop();
+                  Print((mSymbolCount + 1).ToString() + " " + Symbols[mSymbolCount] + " invalid");
+                  mSymbolsCount--;
                }
+            }
+
+            mSymbolCount++;
+         }
+         else if (mSymInfos[^1].SpreadTickCount < 100)
+         {
+            for (int i = 0; i < mSymInfos.Count; i++)
+            {
+               mSymInfos[i].SpreadSum += mSymInfos[i].Symbol.Spread;
+               mSymInfos[i].SpreadTickCount++;
+
+               mSymInfos[i].AvgSpread = mSymInfos[i].SpreadSum / mSymInfos[i].SpreadTickCount;
+            }
+         }
+         else if (mWriteCount < mSymInfos.Count)
+         {
+            // write Zorro Assets file i.e Assets_Pepperstone_Live.csv
+            if (null == mZorroAssetsWriter)
+            {
+               var zorroAssetsPath = Path.Combine(ZorroHistoryPath, "Assets_"
+                  + Account.BrokerName + "_" + (Account.IsLive ? "Live" : "Demo") + ".csv");
+               mZorroAssetsWriter = new StreamWriter(File.OpenWrite(zorroAssetsPath));
+               mZorroAssetsWriter.WriteLine("Name,Price,Spread,RollLong,RollShort,PIP,PIPCost,MarginCost,Market,Multiplier,Commission,Symbol,Leverage,Lotsize,Base,Quote");
+            }
+
+            Print((mWriteCount + 1).ToString() + " Writing " + mSymInfos[mWriteCount].Symbol.Name);
+
+            var digits = mSymInfos[mWriteCount].Symbol.Digits;
+            var line = mSymInfos[mWriteCount].Symbol.Name                                        // User symbol name
+               + "," + mSymInfos[mWriteCount].Symbol.Bid.ToString($"F{digits}", UsCulture)       // Price
+               + "," + mSymInfos[mWriteCount].AvgSpread.ToString($"F{digits + 1}", UsCulture)    // Spread
+               + "," + mSymInfos[mWriteCount].Symbol.SwapLong.ToString($"F{5}", UsCulture)       // Swap long
+               + "," + mSymInfos[mWriteCount].Symbol.SwapShort.ToString($"F{5}", UsCulture)      // Swap short
+               + "," + mSymInfos[mWriteCount].Symbol.PipSize.ToString($"F{digits}", UsCulture)   // Pip size
+               + "," + (mSymInfos[mWriteCount].Symbol.PipValue * mSymInfos[mWriteCount].Symbol.LotSize)    // Value of 1 pip profit or loss per lot
+                  .ToString($"F{8}", UsCulture)
+               + "," + mSymInfos[mWriteCount].Symbol.GetEstimatedMargin(TradeType.Buy, mSymInfos[mWriteCount].Symbol.LotSize)   // Margin
+                  .ToString($"F{2}", UsCulture)
+               // Market open hours ZZZ:HHMM-HHMM, for instance EST:0930-1545
+               + ",UTC:" + mSymInfos[mWriteCount].Symbol.MarketHours.Sessions[1].StartTime.ToString(@"hh\:mm")
+                  + "-" + mSymInfos[mWriteCount].Symbol.MarketHours.Sessions[1].EndTime.ToString(@"hh\:mm")
+               + "," + mSymInfos[mWriteCount].Symbol.VolumeInUnitsMin.ToString($"F{8}", UsCulture)  // Min volume
+               + "," + mSymInfos[mWriteCount].Symbol.Commission.ToString($"F{2}", UsCulture)     // Commission
+               + "," + mSymInfos[mWriteCount].Symbol                                             // Broker symbol Name
+               + "," + ((mSymInfos[mWriteCount].Symbol.DynamicLeverage != null
+                        && mSymInfos[mWriteCount].Symbol.DynamicLeverage.Count > 0
+                           ? (int)mSymInfos[mWriteCount].Symbol.DynamicLeverage[0].Leverage       // Leverage
+                           : ""))
+               + "," + mSymInfos[mWriteCount].Symbol.LotSize.ToString($"F{2}", UsCulture)        // LotSize
+               + "," + mSymInfos[mWriteCount].Symbol.BaseAsset                                   // Base currency
+               + "," + mSymInfos[mWriteCount].Symbol.QuoteAsset;                                 // Quote currency
+
+            mZorroAssetsWriter.WriteLine(line);
+            mWriteCount++;
+         }
+         else
+         {
+            mZorroAssetsWriter.Close();
+            Stop();
+         }
 
          #region Comment
          if (RunningMode.VisualBacktesting == RunningMode
